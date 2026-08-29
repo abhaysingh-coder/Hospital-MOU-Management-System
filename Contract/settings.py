cf9076707be31aec3ec12f0e1556148292181447
abhaysingh-coder <srinetabhaysingh14@gmail.com>
2026-08-29 08:18:30 +0530

    fix: Correct CSS file path casing in templates and improve login form structure

--- Contract/settings.py
+++ Contract/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / 'App' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     'default': {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
